6bb5df6022c7303de165d7deb345f02d98f2ecfe
Jack <jack200034@hotmail.com>
2023-05-06 22:23:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const people = {
+    "people": [
+        {
+            id: 1,
+            name: "Jack",
+            age: 22
+        },
+        {
+            id: 2,
+            name: "Jane",
+            age: 32
+        },
+        {
+            id: 3,
+            name: "John",
+            age: 42
+        }
+    ]
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const peopleTemplate = document.getElementById("people-template").innerHTML;
+    const compiledPeopleTemplate = Handlebars.compile(peopleTemplate);
+    document.querySelector(".people-list-container").innerHTML = compiledPeopleTemplate(people.people[0]);
+
+});
